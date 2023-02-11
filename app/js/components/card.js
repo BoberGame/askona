4772a5a28ca0796fb85eec30348cb6456a0a5cd1
BoberGame <pankov.zhora@gmail.com>
2023-02-11 19:38:47 +1100

    create dist branch

--- app/js/components/card.js
+++
@@ -1,9 +0,0 @@
-const changeIconColor = () => {
-  document.addEventListener('click', (e) => {
-    const btn = e.target.closest('.product-card__mobileButtons-button');
-    if (btn) {
-      btn.classList.toggle('active');
-    }
-  });
-};
-changeIconColor();
